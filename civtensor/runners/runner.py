c64d782672178a846bec2ecb31b6135687ce3772
Yifan Zhong <xiaofanzhong226@126.com>
2023-08-31 11:29:35 +0800

    implement evaluation

--- civtensor/runners/runner.py
+++ civtensor/runners/runner.py
@@ -10,6 +10,7 @@ from civtensor.envs.freeciv_tensor_env.freeciv_tensor_logger import FreecivTenso
 from civtensor.utils.envs_tools import set_seed, make_train_env, make_eval_env
 from civtensor.utils.models_tools import init_device
 from civtensor.utils.configs_tools import init_dir, save_config
+from civtensor.utils.trans_tools import _t2n
 
 
 class Runner:
@@ -144,6 +145,21 @@ class Runner:
                         self.buffer.masks[step],
                         deterministic=False,
                     )
+
+                actor_type = _t2n(actor_type)
+                actor_type_log_prob = _t2n(actor_type_log_prob)
+                city_id = _t2n(city_id)
+                city_id_log_prob = _t2n(city_id_log_prob)
+                city_action_type = _t2n(city_action_type)
+                city_action_type_log_prob = _t2n(city_action_type_log_prob)
+                unit_id = _t2n(unit_id)
+                unit_id_log_prob = _t2n(unit_id_log_prob)
+                unit_action_type = _t2n(unit_action_type)
+                unit_action_type_log_prob = _t2n(unit_action_type_log_prob)
+                gov_action_type = _t2n(gov_action_type)
+                gov_action_type_log_prob = _t2n(gov_action_type_log_prob)
+                value_pred = _t2n(value_pred)
+                rnn_hidden_state = _t2n(rnn_hidden_state)
 
                 (
                     rules,
@@ -356,7 +372,199 @@ class Runner:
 
     @torch.no_grad()
     def eval(self):
-        raise NotImplementedError
+        """Evaluate the model."""
+        self.logger.eval_init()  # logger callback at the beginning of evaluation
+        eval_episode = 0
+
+        (
+            rules,
+            player,
+            other_players,
+            units,
+            cities,
+            other_units,
+            other_cities,
+            map,
+            other_players_mask,
+            units_mask,
+            cities_mask,
+            other_units_mask,
+            other_cities_mask,
+            actor_type_mask,
+            city_id_mask,
+            city_action_type_mask,
+            unit_id_mask,
+            unit_action_type_mask,
+            gov_action_type_mask,
+        ) = self.eval_envs.reset()
+
+        eval_rnn_hidden_state = np.zeros(
+            (
+                self.algo_args["train"]["n_eval_rollout_threads"],
+                self.n_rnn_layers,
+                self.rnn_hidden_dim,
+            ),
+            dtype=np.float32,
+        )
+
+        eval_mask = np.ones(
+            (self.algo_args["train"]["n_eval_rollout_threads"], 1), dtype=np.float32
+        )
+
+        while True:
+            (
+                actor_type,
+                actor_type_log_prob,
+                city_id,
+                city_id_log_prob,
+                city_action_type,
+                city_action_type_log_prob,
+                unit_id,
+                unit_id_log_prob,
+                unit_action_type,
+                unit_action_type_log_prob,
+                gov_action_type,
+                gov_action_type_log_prob,
+                value_pred,
+                eval_rnn_hidden_state,
+            ) = self.algo.agent(
+                rules,
+                player,
+                other_players,
+                units,
+                cities,
+                other_units,
+                other_cities,
+                map,
+                other_players_mask,
+                units_mask,
+                cities_mask,
+                other_units_mask,
+                other_cities_mask,
+                actor_type_mask,
+                city_id_mask,
+                city_action_type_mask,
+                unit_id_mask,
+                unit_action_type_mask,
+                gov_action_type_mask,
+                eval_rnn_hidden_state,
+                eval_mask,  # TODO check whether logic related to mask is correct
+                deterministic=True,
+            )
+
+            actor_type = _t2n(actor_type)
+            actor_type_log_prob = _t2n(actor_type_log_prob)
+            city_id = _t2n(city_id)
+            city_id_log_prob = _t2n(city_id_log_prob)
+            city_action_type = _t2n(city_action_type)
+            city_action_type_log_prob = _t2n(city_action_type_log_prob)
+            unit_id = _t2n(unit_id)
+            unit_id_log_prob = _t2n(unit_id_log_prob)
+            unit_action_type = _t2n(unit_action_type)
+            unit_action_type_log_prob = _t2n(unit_action_type_log_prob)
+            gov_action_type = _t2n(gov_action_type)
+            gov_action_type_log_prob = _t2n(gov_action_type_log_prob)
+            value_pred = _t2n(value_pred)
+            eval_rnn_hidden_state = _t2n(eval_rnn_hidden_state)
+
+            (
+                rules,
+                player,
+                other_players,
+                units,
+                cities,
+                other_units,
+                other_cities,
+                map,
+                other_players_mask,
+                units_mask,
+                cities_mask,
+                other_units_mask,
+                other_cities_mask,
+                actor_type_mask,
+                city_id_mask,
+                city_action_type_mask,
+                unit_id_mask,
+                unit_action_type_mask,
+                gov_action_type_mask,
+                reward,
+                term,
+                trunc,
+            ) = self.eval_envs.step(
+                actor_type,
+                city_id,
+                city_action_type,
+                unit_id,
+                unit_action_type,
+                gov_action_type,
+            )  # no info at the moment
+
+            # mask: 1 if not done, 0 if done
+            done = np.logical_or(term, trunc)  # (n_rollout_threads, 1)
+            mask = np.logical_not(done)  # (n_rollout_threads, 1)
+
+            # bad_mask use 0 to denote truncation and 1 to denote termination or not done
+            bad_mask = np.logical_not(trunc)
+
+            # reset certain rnn hidden state
+            done_env = done.squeeze(1)
+            eval_rnn_hidden_state[done_env == True] = np.zeros(
+                (
+                    (done_env == True).sum(),
+                    self.n_rnn_layers,
+                    self.rnn_hidden_dim,
+                )
+            )
+
+            eval_data = (
+                rules,
+                player,
+                other_players,
+                units,
+                cities,
+                other_units,
+                other_cities,
+                map,
+                other_players_mask,
+                units_mask,
+                cities_mask,
+                other_units_mask,
+                other_cities_mask,
+                eval_rnn_hidden_state,
+                actor_type,
+                actor_type_log_prob,
+                actor_type_mask,
+                city_id,
+                city_id_log_prob,
+                city_id_mask,
+                city_action_type,
+                city_action_type_log_prob,
+                city_action_type_mask,
+                unit_id,
+                unit_id_log_prob,
+                unit_id_mask,
+                unit_action_type,
+                unit_action_type_log_prob,
+                unit_action_type_mask,
+                gov_action_type,
+                gov_action_type_log_prob,
+                gov_action_type_mask,
+                mask,
+                bad_mask,
+                reward,
+                value_pred,
+            )
+
+            self.logger.eval_per_step(eval_data)
+
+            for eval_i in range(self.algo_args["train"]["n_eval_rollout_threads"]):
+                if done[eval_i][0]:
+                    eval_episode += 1
+                    self.logger.eval_thread_done(eval_i)
+
+            if eval_episode >= self.algo_args["train"]["eval_episodes"]:
+                self.logger.eval_log(eval_episode)
+                break
 
     def prep_training(self):
         """Prepare for training."""
--- civtensor/utils/trans_tools.py
+++ civtensor/utils/trans_tools.py
@@ -1,3 +1,8 @@
+def _t2n(value):
+    """Convert torch.Tensor to numpy.ndarray."""
+    return value.detach().cpu().numpy()
+
+
 def _flatten(T, N, value):
     """Flatten the first two dimensions of a tensor."""
     return value.reshape(T * N, *value.shape[2:])
