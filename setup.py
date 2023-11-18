3b9f45c5bd0db04bc5aa0b3cf3f02152e8bf1255
Siyuan Qi <syqi@ucla.edu>
2023-11-18 17:16:22 +0800

    Fix setup error for tensorboardX.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "pyyaml>=5.3.1",
         "tensorboard",
         "setproctitle",
-        "tensorboardX@https://github.com/lanpa/tensorboardX.git@master",
+        "tensorboardX@git+https://github.com/lanpa/tensorboardX.git@master",
     ],
     setup_requires=[
     ],
